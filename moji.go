9a94e85bbf53a3cae0d67732e3364509bf561b6b
Hualet Wang <mr.asianwang@gmail.com>
2017-03-28 13:39:31 +0800

    parse forecasts done

--- moji.go
+++ moji.go
@@ -11,12 +11,14 @@ import (
 )
 
 const (
-	mojiHost         = "http://aliv1.data.moji.com"
-	briefForcast     = "/whapi/json/aliweather/briefforecast6days"
-	briefCurrent     = "/whapi/json/aliweather/briefcondition"
-	token            = "0f9d7e535dfbfad15b8fd2a84fee3e36"
-	appCodeEnv       = "MOJI_APP_CODE"
-	updateTimeLayout = "2006-01-02 15:04:00"
+	mojiHost     = "http://aliv1.data.moji.com"
+	briefForcast = "/whapi/json/aliweather/briefforecast6days"
+	briefCurrent = "/whapi/json/aliweather/briefcondition"
+	token        = "0f9d7e535dfbfad15b8fd2a84fee3e36"
+	appCodeEnv   = "MOJI_APP_CODE"
+
+	updateTimeLayout  = "2006-01-02 15:04:05"
+	predictDateLayout = "2006-01-02"
 )
 
 // City represents a city in moji service.
@@ -40,7 +42,7 @@ type Condition struct {
 
 // Forecast stands for one forecast records of the future days.
 type Forecast struct {
-	PredictDate    time.Time `json:"predictDate"`
+	PredictDate    time.Time
 	DayCondition   *Condition
 	NightCondition *Condition
 }
@@ -132,31 +134,44 @@ func (m *Client) ForecastByLatLong(lat, long string) (*ForecastData, error) {
 // UnmarshalJSON unmarshals Forecast from json data.
 func (f *Forecast) UnmarshalJSON(data []byte) error {
 	conv := &struct {
-		*Forecast
-		ConditionDay       string    `json:"conditionDay"`
-		ConditionNight     string    `json:"conditionNight"`
-		ConditionIDDay     int       `json:"conditionIdDay"`
-		ConditionIDNight   int       `json:"conditionIdNight"`
-		UpdateTime         time.Time `json:"updatetime"`
-		TempDay            int       `json:"tempDay"`
-		TempNight          int       `json:"tempNight"`
-		WindDirectionDay   string    `json:"windDirDay"`
-		WindDirectionNight string    `jsong:"windDirNight"`
-		WindLevelDay       int       `json:"windLevelDay"`
-		WindLevelNight     int       `json:"windLevelNight"`
+		PredictDate        string `json:"predictDate"`
+		ConditionDay       string `json:"conditionDay"`
+		ConditionNight     string `json:"conditionNight"`
+		ConditionIDDay     int    `json:"conditionIdDay,string"`
+		ConditionIDNight   int    `json:"conditionIdNight,string"`
+		UpdateTime         string `json:"updatetime"`
+		TempDay            int    `json:"tempDay,string"`
+		TempNight          int    `json:"tempNight,string"`
+		WindDirectionDay   string `json:"windDirDay"`
+		WindDirectionNight string `json:"windDirNight"`
+		WindLevelDay       int    `json:"windLevelDay,string"`
+		WindLevelNight     int    `json:"windLevelNight,string"`
 	}{}
-	err := json.Unmarshal(data, conv)
 
+	err := json.Unmarshal(data, conv)
 	if err != nil {
 		return err
 	}
+
+	tm := time.Now()
+	updateTime, err := time.ParseInLocation(updateTimeLayout, conv.UpdateTime, tm.Location())
+	if err != nil {
+		return err
+	}
+
+	predictDate, err := time.ParseInLocation(predictDateLayout, conv.PredictDate, tm.Location())
+	if err != nil {
+		return err
+	}
+
+	f.PredictDate = predictDate
 
 	f.DayCondition = &Condition{
 		ID:            conv.ConditionIDDay,
 		Name:          conv.ConditionDay,
 		Humidity:      0,
 		Temp:          conv.TempDay,
-		UpdateTime:    conv.UpdateTime,
+		UpdateTime:    updateTime,
 		WindDirection: conv.WindDirectionDay,
 		WindLevel:     conv.WindLevelDay,
 	}
@@ -166,7 +181,7 @@ func (f *Forecast) UnmarshalJSON(data []byte) error {
 		Name:          conv.ConditionNight,
 		Humidity:      0,
 		Temp:          conv.TempNight,
-		UpdateTime:    conv.UpdateTime,
+		UpdateTime:    updateTime,
 		WindDirection: conv.WindDirectionNight,
 		WindLevel:     conv.WindLevelNight,
 	}
@@ -213,9 +228,11 @@ func unmarshalForecastData(content []byte) (*ForecastData, error) {
 	ret := &ForecastData{}
 
 	val := struct {
-		*ForecastData
-		Code int `json:"code"`
-	}{ret, 0}
+		ForecastData *ForecastData `json:"data"`
+		Code         int           `json:"code"`
+	}{
+		ForecastData: ret,
+	}
 
 	err := json.Unmarshal(content, &val)
 	if err != nil {
--- moji_test.go
+++ moji_test.go
@@ -86,3 +86,167 @@ func TestUnmarshalConditionData(t *testing.T) {
 		t.Errorf("parsed condition not correct")
 	}
 }
+
+func TestUnmarshalForecastData(t *testing.T) {
+	data := []byte(
+		`{
+            "code": 0,
+            "data": {
+                "city": {
+                "cityId": 284609,
+                "counname": "中国",
+                "name": "东城区",
+                "pname": "北京市"
+                },
+                "forecast": [
+                {
+                    "conditionDay": "多云",
+                    "conditionIdDay": "1",
+                    "conditionIdNight": "31",
+                    "conditionNight": "多云",
+                    "predictDate": "2016-09-01",
+                    "tempDay": "27",
+                    "tempNight": "18",
+                    "updatetime": "2016-09-01 09:07:08",
+                    "windDirDay": "西北风",
+                    "windDirNight": "西北风",
+                    "windLevelDay": "3",
+                    "windLevelNight": "2"
+                },
+                {
+                    "conditionDay": "多云",
+                    "conditionIdDay": "1",
+                    "conditionIdNight": "31",
+                    "conditionNight": "多云",
+                    "predictDate": "2016-09-02",
+                    "tempDay": "27",
+                    "tempNight": "20",
+                    "updatetime": "2016-09-01 09:07:08",
+                    "windDirDay": "北风",
+                    "windDirNight": "东南风",
+                    "windLevelDay": "2",
+                    "windLevelNight": "1"
+                },
+                {
+                    "conditionDay": "多云",
+                    "conditionIdDay": "1",
+                    "conditionIdNight": "31",
+                    "conditionNight": "多云",
+                    "predictDate": "2016-09-03",
+                    "tempDay": "28",
+                    "tempNight": "20",
+                    "updatetime": "2016-09-01 09:07:08",
+                    "windDirDay": "南风",
+                    "windDirNight": "东风",
+                    "windLevelDay": "2",
+                    "windLevelNight": "2"
+                },
+                {
+                    "conditionDay": "局部阵雨",
+                    "conditionIdDay": "3",
+                    "conditionIdNight": "31",
+                    "conditionNight": "多云",
+                    "predictDate": "2016-09-04",
+                    "tempDay": "28",
+                    "tempNight": "18",
+                    "updatetime": "2016-09-01 09:07:08",
+                    "windDirDay": "南风",
+                    "windDirNight": "西风",
+                    "windLevelDay": "2",
+                    "windLevelNight": "2"
+                },
+                {
+                    "conditionDay": "多云",
+                    "conditionIdDay": "1",
+                    "conditionIdNight": "31",
+                    "conditionNight": "少云",
+                    "predictDate": "2016-09-05",
+                    "tempDay": "30",
+                    "tempNight": "21",
+                    "updatetime": "2016-09-01 09:07:08",
+                    "windDirDay": "西北风",
+                    "windDirNight": "西北风",
+                    "windLevelDay": "2",
+                    "windLevelNight": "2"
+                },
+                {
+                    "conditionDay": "多云",
+                    "conditionIdDay": "1",
+                    "conditionIdNight": "31",
+                    "conditionNight": "多云",
+                    "predictDate": "2016-09-06",
+                    "tempDay": "30",
+                    "tempNight": "20",
+                    "updatetime": "2016-09-01 09:07:08",
+                    "windDirDay": "西北风",
+                    "windDirNight": "西风",
+                    "windLevelDay": "3",
+                    "windLevelNight": "2"
+                }
+                ]
+            },
+            "msg": "success",
+            "rc": {
+                "c": 0,
+                "p": "success"
+            }
+        }`)
+
+	ret, err := unmarshalForecastData(data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	expCity := City{
+		ID:       284609,
+		Name:     "东城区",
+		County:   "中国",
+		Province: "北京市",
+	}
+
+	if ret.City != expCity {
+		t.Error("parsed city not correct")
+	}
+
+	if len(ret.Forecasts) != 6 {
+		t.Error("forecasts count not match 6")
+	}
+
+	now := time.Now()
+	loc := now.Location()
+
+	expDate := time.Date(2016, time.September, 1, 0, 0, 0, 0, loc)
+
+	expDayCondition := Condition{
+		ID:            1,
+		Name:          "多云",
+		Humidity:      0,
+		Temp:          27,
+		UpdateTime:    time.Date(2016, time.September, 1, 9, 7, 8, 0, loc),
+		WindDirection: "西北风",
+		WindLevel:     3,
+	}
+
+	expNightCondition := Condition{
+		ID:            31,
+		Name:          "多云",
+		Humidity:      0,
+		Temp:          18,
+		UpdateTime:    time.Date(2016, time.September, 1, 9, 7, 8, 0, loc),
+		WindDirection: "西北风",
+		WindLevel:     2,
+	}
+
+	first := ret.Forecasts[0]
+	if first.PredictDate != expDate {
+		t.Error("parsed predict date not correct")
+	}
+
+	if *first.DayCondition != expDayCondition {
+		t.Error("parsed day condition not correct")
+	}
+
+	if *first.NightCondition != expNightCondition {
+		t.Error("parsed night condition not correct")
+	}
+}
